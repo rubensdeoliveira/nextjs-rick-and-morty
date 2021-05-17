f5b790cc7cbb1d9fe54d6cbab85f5be9753f82b6
rubensojunior <rubensojunior6@gmail.com>
2021-05-17 17:51:27 -0300

    feat: implementing global styles

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -8,11 +8,21 @@ export default createGlobalStyle`
   }
 
   :root {
+    --black: #000000;
     --white: #ffffff;
+
+    --blue-600: #00A5C4;
 
     --yellow-400: #CBD736;
 
+    --gray-100: #D3D3D3;
     --gray-300: #9F9F9F;
+    --gray-400: #909090;
+    --gray-500: #8C8C8C;
+    --gray-600: #606060;
+    --gray-700: #3e3e3e;
+    --gray-750: #363636;
+    --gray-800: #1E1E1E;
     --gray-900: #121214;
   }
 
