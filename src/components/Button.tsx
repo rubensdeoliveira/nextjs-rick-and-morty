2c0207c5ec72f814d7bbccceaf327f1e2c7c94e4
rubensojunior <rubensojunior6@gmail.com>
2021-05-17 17:47:58 -0300

    feat: implementing button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,15 @@
-import { ReactNode } from 'react'
+import { ButtonHTMLAttributes, ReactNode } from 'react'
 
-interface ButtonProps {
+import { Container } from '../styles/components/Button'
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode
 }
 
-export function Button({ children }: ButtonProps) {
-  return <button>{children}</button>
+export function Button({ children, ...rest }: ButtonProps) {
+  return (
+    <Container type="button" {...rest}>
+      {children}
+    </Container>
+  )
 }
