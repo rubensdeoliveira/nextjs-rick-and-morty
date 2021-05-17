343835bda5d49a7603129571eb3ee85ac0627929
rubensojunior <rubensojunior6@gmail.com>
2021-05-17 17:54:17 -0300

    feat: implementing home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,132 @@
+import { useCallback, useState } from 'react'
 import { Button } from '../components/Button'
+import { Character } from '../components/Character'
 import { Input } from '../components/Input'
-import { Background } from '../styles/pages/Home'
+import { Loading } from '../components/Loading'
+import { Logo } from '../components/Logo'
+import { ModalInfo } from '../components/Modal/ModalInfo'
+import { ModalViewCharacter } from '../components/Modal/ModalViewCharacter'
+import { Pagination } from '../components/Pagination'
+import {
+  Character as CharacterItem,
+  Characters,
+} from '../interfaces/Characters'
+import { api } from '../services/api'
+import {
+  Wrapper,
+  Container,
+  LogoContainer,
+  SearchContainer,
+  CharactersList,
+} from '../styles/pages/Home'
 
 export default function Home() {
+  const [loading, setLoading] = useState<boolean>(false)
+  const [search, setSearch] = useState('')
+  const [characters, setCharacters] = useState<Characters>({} as Characters)
+  const [selectedCharacter, setSelectedCharacter] = useState<CharacterItem>(
+    {} as CharacterItem,
+  )
+  const [page, setPage] = useState(1)
+  const [modalOpen, setModalOpen] = useState(false)
+  const [modalInfoOpen, setModalInfoOpen] = useState(false)
+
+  const handleSetInitialState = useCallback(() => {
+    setCharacters({} as Characters)
+    setSelectedCharacter({} as CharacterItem)
+    setSearch('')
+    setPage(1)
+    setLoading(false)
+  }, [])
+
+  const toggleModal = useCallback(() => {
+    setModalOpen(!modalOpen)
+  }, [modalOpen])
+
+  const toggleInfoModal = useCallback(() => {
+    setModalInfoOpen(!modalInfoOpen)
+  }, [modalInfoOpen])
+
+  const handleSelectCharacter = useCallback((character: CharacterItem) => {
+    setSelectedCharacter(character)
+    setModalOpen(true)
+  }, [])
+
+  const handleShowModalInfo = useCallback(() => {
+    setModalInfoOpen(true)
+  }, [])
+
+  const handleSearch = useCallback(
+    async (newPage = 1) => {
+      try {
+        setLoading(true)
+        setCharacters({} as Characters)
+        setPage(newPage)
+
+        const { data } = await api.get(
+          `character?name=${search.trim()}&page=${newPage}`,
+        )
+
+        setCharacters(data)
+
+        setLoading(false)
+      } catch (err) {
+        setLoading(false)
+        handleShowModalInfo()
+      }
+    },
+    [search, handleShowModalInfo],
+  )
+
   return (
-    <>
-      <Background>
-        <Input placeholder="Search characters" />
-        <Button>Entrar</Button>
-      </Background>
-    </>
+    <Wrapper>
+      {loading && <Loading />}
+
+      <Container listShowing={!!characters.results} isLoading={loading}>
+        <ModalViewCharacter
+          isOpen={modalOpen}
+          setIsOpen={toggleModal}
+          character={selectedCharacter}
+        />
+
+        <ModalInfo isOpen={modalInfoOpen} setIsOpen={toggleInfoModal} />
+
+        <LogoContainer onClick={handleSetInitialState}>
+          <Logo />
+        </LogoContainer>
+
+        {!loading && (
+          <SearchContainer>
+            <Input
+              value={search}
+              placeholder="Search characters"
+              onChange={e => {
+                setSearch(e.target.value)
+              }}
+            />
+            <Button onClick={() => handleSearch()}>Search</Button>
+          </SearchContainer>
+        )}
+
+        {characters.results && (
+          <>
+            <CharactersList>
+              {characters.results.map(character => (
+                <Character
+                  key={character.id}
+                  character={character}
+                  onSelectCharacter={handleSelectCharacter}
+                />
+              ))}
+            </CharactersList>
+            <Pagination
+              totalCountOfRegisters={characters.info.count}
+              currentPage={page}
+              onPageChange={handleSearch}
+            />
+          </>
+        )}
+      </Container>
+    </Wrapper>
   )
 }
--- src/styles/pages/Home.ts
+++ src/styles/pages/Home.ts
@@ -1,10 +1,74 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 import backgroundImage from '../../assets/background.svg'
 
-export const Background = styled.div`
-  width: 100vw;
-  height: 100vh;
-  background: url(${backgroundImage}) no-repeat;
+interface ContainerProps {
+  listShowing: boolean
+  isLoading: boolean
+}
+
+export const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  position: relative;
+`
+
+export const Container = styled.div<ContainerProps>`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  width: 100%;
+
+  ${props =>
+    props.listShowing
+      ? css`
+          height: 100%;
+        `
+      : css`
+          height: 100vh;
+        `}
+
+  background-image: url(${backgroundImage});
+  background-repeat: no-repeat;
   background-size: cover;
+  background-position: center;
+  background-attachment: fixed;
+  background-color: var(--gray-750);
+
+  padding: 2.375rem;
+
+  ${props =>
+    props.isLoading &&
+    css`
+      filter: blur(1.9rem);
+      -webkit-filter: blur(1.9rem);
+    `}
+`
+
+export const LogoContainer = styled.button`
+  background: transparent;
+  border: 0;
+`
+
+export const SearchContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 4.625rem;
+
+  button {
+    margin-left: 1rem;
+  }
+`
+
+export const CharactersList = styled.ul`
+  width: 100%;
+  max-width: 968px;
+  margin: 6.3125rem auto;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 1.5rem;
 `
