674ad9798b19fb82e04ff1563fb417c104b70167
rubensojunior <rubensojunior6@gmail.com>
2021-05-17 17:48:08 -0300

    feat: implementing character component

---
+++ src/components/Character.tsx
@@ -0,0 +1,29 @@
+import { Character as CharacterItem } from '../interfaces/Characters'
+import {
+  Container,
+  Image,
+  TextContainer,
+  Name,
+  Specie,
+} from '../styles/components/Character'
+
+interface CharacterProps {
+  character: CharacterItem
+  onSelectCharacter: (character: CharacterItem) => void
+}
+
+export function Character({ character, onSelectCharacter }: CharacterProps) {
+  return (
+    <Container
+      onClick={() => {
+        onSelectCharacter(character)
+      }}
+    >
+      <Image src={character.image} alt={character.name} />
+      <TextContainer>
+        <Name>{character.name}</Name>
+        <Specie>{character.species}</Specie>
+      </TextContainer>
+    </Container>
+  )
+}
