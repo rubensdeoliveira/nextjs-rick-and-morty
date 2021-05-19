9645a4776ef0f01bd571b5d72a58ebcd3da7f263
rubensojunior <rubensojunior6@gmail.com>
2021-05-19 19:21:57 -0300

    test: implementing input tests

---
+++ src/components/Input/Input.spec.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react'
+import { Input } from '.'
+
+describe('<Input />', () => {
+  it('should render input correctly', () => {
+    render(<Input />)
+
+    const input = screen.getByRole('textbox')
+
+    expect(input).toBeInTheDocument()
+  })
+})
