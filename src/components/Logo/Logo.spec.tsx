51e19df79a37d0a604ce3e148e81c00882f0281f
rubensojunior <rubensojunior6@gmail.com>
2021-05-19 19:22:26 -0300

    test: implementing logo tests

---
+++ src/components/Logo/Logo.spec.tsx
@@ -0,0 +1,22 @@
+/* eslint-disable react/display-name */
+import { render, screen } from '@testing-library/react'
+import { Logo } from '.'
+
+jest.mock('next/image', () => ({
+  __esModule: true,
+  default: (props: any) => {
+    return <img {...props} />
+  },
+}))
+
+describe('<Logo />', () => {
+  it('should render logo correctly', () => {
+    render(<Logo />)
+
+    const logo = screen.getByRole('img', {
+      name: /rick and morty/i,
+    })
+
+    expect(logo).toBeInTheDocument()
+  })
+})
